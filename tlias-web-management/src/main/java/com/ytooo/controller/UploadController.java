1eb6a1e32da6d5cdb14593b81d9d14f82aae6596
863978745@qq.com <863978745@qq.com>
2025-11-05 22:33:52 +0800

    Java Learn

---
+++ tlias-web-management/src/main/java/com/ytooo/controller/UploadController.java
@@ -0,0 +1,16 @@
+package com.ytooo.controller;
+
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Slf4j
+@RestController
+public class UploadController {
+
+    @PostMapping("/upload")
+    public void upload(){
+
+    }
+}
