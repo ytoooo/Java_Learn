3c7d0e0ef7721e0158f689a08d0facce91fa0513
863978745@qq.com <863978745@qq.com>
2025-11-05 23:01:02 +0800

    Java Learn

--- tlias-web-management/src/main/java/com/ytooo/controller/UploadController.java
+++ tlias-web-management/src/main/java/com/ytooo/controller/UploadController.java
@@ -1,16 +1,19 @@
 package com.ytooo.controller;
 
 
+import com.ytooo.pojo.Result;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 
 @Slf4j
 @RestController
 public class UploadController {
 
     @PostMapping("/upload")
-    public void upload(){
-
+    public Result upload(String name, Integer age, MultipartFile file){
+        log.info("接收参数：{}，{}，{}", name, age, file);
+        return Result.success();
     }
 }
